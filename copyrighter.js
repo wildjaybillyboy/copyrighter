42b5f3d4ac5403c50cda6ee9a943a1f530fdcd00
wildjaybillyboy <orinshigeoka@orin-shigeokas-macbook-pro.local>
2015-09-03 19:54:21 -0700

    add try catch

--- copyrighter.js
+++ copyrighter.js
@@ -5,27 +5,38 @@ Meteor.methods ({
     //single call to set copyright parameters
     'setCopyright': function (data) {
         //check name is not null and log if no name was entered
-        if (data.name) {
-            copyright.name = data.name;
-        } else {
+        try {
+            if (data.name) {
+                copyright.name = data.name;
+            }
+        } catch (error) {
             console.log('please enter the owners name');
+            console.log(error);
         }
         //check for a year, if entered as a string, convert to number
         var dateNumber = data.date *= 1;
-        if (data.date) {
-            if (data.date.toFixed || dateNumber.toFixed) {
-                copyright.date = dateNumber;
-            } else {
-                console.log('please enter a proper 4 digit year');
+        try {
+            if (data.date) {
+                if (data.date.toFixed || dateNumber.toFixed) {
+                    copyright.date = dateNumber;
+                }
             }
+        } catch (error) {
+            console.log('please enter a proper 4 digit year');
+            console.log(error);
         }
         //convert to unicode
-        if (data.mark) {
-            if (data.mark === "copyright") {
-                copyright.mark = "&copy;";
-            } else if (data.mark === "trademark") {
-                copyright.mark = "&reg;";
+        try {
+            if (data.mark) {
+                if (data.mark === "copyright") {
+                    copyright.mark = "&copy;";
+                } else if (data.mark === "trademark") {
+                    copyright.mark = "&reg;";
+                }
             }
+        } catch (error) {
+            console.log('please enter copyright or trademark');
+            console.log(error);
         }
         //css style can be a defined style or a user created style
         copyright.style = data.style || "small-copyright";
